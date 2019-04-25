e7d2c99ff418c01953121f0ee3c1377fe241cc9e
codingMummy <aleksandratychulska@gmail.com>
2019-04-25 10:27:48 +0100

    added .addEventListener

--- js/script.js
+++ js/script.js
@@ -1,10 +1,10 @@
 'use strict';
 
 function titleClickHandler(event) {
+    const clickedElement = this;
     console.log('Link was clicked!');
-    console.log(event);
 
-    /* remove class 'active' from all article links  */
+    /* [DONE] remove class 'active' from all article links  */
 
     const activeLinks = document.querySelectorAll('.titles a.active');
 
@@ -13,9 +13,13 @@ function titleClickHandler(event) {
     }
 
 
-    /* add class 'active' to the clicked link */
+    /* [IN PROGRESS] add class 'active' to the clicked link */
 
-    /* remove class 'active' from all articles */
+    console.log('clickedElement:', clickedElement);
+    activeLink.classList.addEventListener('active');
+
+
+    /* [DONE] remove class 'active' from all articles */
 
     const activeArticles = document.querySelectorAll('.posts article.active');
 
